d3f4f969f92a771c9ae43003310b03b6dec2198c
Wendel Cutrim <wendelcutrim@hotmail.com>
2023-02-02 13:48:19 -0300

    feat: Finalizado header e está com responsividade

---
+++ public/js/main.js
@@ -0,0 +1,35 @@
+const width = window.innerWidth;
+const menuButton = document.querySelector("#open-menu");
+const closeMenuButton = document.querySelector("#close-menu");
+const nav = document.querySelector("nav");
+
+if (width <= 768) {
+    menuButton.classList.add("active")
+}
+
+menuButton.addEventListener("click", handleMenuMobile);
+closeMenuButton.addEventListener("click", handleMenuMobile);
+
+function handleMenuMobile(evt) {
+    nav.classList.add("active");
+    console.log(evt.target.textContent);
+
+    if (evt.target.textContent === " menu ") {
+        if (menuButton.classList.contains("active")) {
+            menuButton.classList.remove("active");
+
+            closeMenuButton.classList.add("active");
+        }
+    }
+
+    if (evt.target.textContent === " close ") {
+        if (closeMenuButton.classList.contains("active")) {
+            closeMenuButton.classList.remove("active");
+
+            menuButton.classList.add("active");
+            nav.classList.remove("active");
+
+        }
+    }
+
+}
